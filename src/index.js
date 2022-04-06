8dffcd2ffbdc8031582565070b05bc2e5535f5fc
Dangelo Arrivillaga <talgidisoluciones@gmail.com>
2022-04-06 18:02:30 -0400

    1st. commit working without linters errors

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import tableScores from './modules/tablescore.js';
 import './styles/index.css';
+
+const containerLeaderboard = document.getElementById('leaderboard');
+
+const leaderBoard = () => {
+  tableScores.forEach((score) => {
+    const scoreRow = document.createElement('p');
+    containerLeaderboard.appendChild(scoreRow);
+    scoreRow.textContent = `${score.name}: ${score.score}`;
+  });
+};
+
+leaderBoard();
